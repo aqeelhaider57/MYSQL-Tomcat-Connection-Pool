778be390ff8415fa738cf1e65ccd3893350591a3
hp <hp@PC>
2019-11-06 16:41:35 +0500

    First commit

---
+++ TomcatConnectionPool/src/main/java/org/data/app/TomcatConnectionPoolApplication.java
@@ -0,0 +1,13 @@
+package org.data.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatConnectionPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomcatConnectionPoolApplication.class, args);
+	}
+
+}
---
+++ TomcatConnectionPool/src/test/java/org/data/app/TomcatConnectionPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.data.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomcatConnectionPoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
