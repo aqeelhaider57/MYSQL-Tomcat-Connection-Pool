7a5cdc2e34bb698680a10b8649fd396744294170
hp <PoweredByAqeelHaider>
2019-11-06 16:47:42 +0500

    First Commit

---
+++ HikariConnectionPool/src/main/java/com/hikaricp/app/HikariConnectionPoolApplication.java
@@ -0,0 +1,13 @@
+package com.hikaricp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikariConnectionPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikariConnectionPoolApplication.class, args);
+	}
+
+}
